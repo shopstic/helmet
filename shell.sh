de48fa9f345f9605304b79704a8e6daecb8b1be2
Jacky Nguyen <nktpro@gmail.com>
2021-04-24 19:35:27 -0400

    Update shell image and push it to Docker Hub for external consumption

--- shell.sh
+++ shell.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SHELL_NAME=${SHELL_NAME:-"helmet-shell"}
 export DOCKER_BUILDKIT=1
 
 docker build ./images/shell
-IMAGE_ID=$(docker build -q ./images/shell)
+IMAGE_ID=$(docker build -q ./images/shell | head -n1)
 
 docker run \
   -it --rm \
   --privileged \
-  --hostname=helmet-shell \
+  --hostname="${SHELL_NAME}" \
   --init \
   -v "${HOME}/.kube:/root/.kube" \
   -v "${DENO_DIR}:/root/.cache/deno" \
