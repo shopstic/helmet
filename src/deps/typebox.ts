ec61c026b0e55a26208f8b9fb4515bba6d5c0f6b
Jacky Nguyen <nktpro@gmail.com>
2021-04-24 01:59:13 -0400

    Change typebox dep to export *

--- src/deps/typebox.ts
+++ src/deps/typebox.ts
@@ -1,6 +1,1 @@
-export { Type } from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.0/src/deps/typebox.ts";
-export type {
-  Static,
-  TObject,
-  TProperties,
-} from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.0/src/deps/typebox.ts";
+export * from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.0/src/deps/typebox.ts";
