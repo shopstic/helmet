4497f5a399896a75ed4bb4e4c987825d6867f2df
Jacky Nguyen <nktpro@gmail.com>
2021-05-10 14:00:41 -0400

    Update dependencies

--- src/deps/async_utils.ts
+++ src/deps/async_utils.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.1/src/async_utils.ts";
+export * from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.2/src/async_utils.ts";
--- src/deps/typebox.ts
+++ src/deps/typebox.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.1/src/deps/typebox.ts";
+export * from "https://raw.githubusercontent.com/shopstic/deno-utils/1.7.2/src/deps/typebox.ts";
