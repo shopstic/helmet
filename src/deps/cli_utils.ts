d6ae84b3d8bec2358c7f88d683fef25b97b9fed7
Jacky Nguyen <nktpro@gmail.com>
2023-09-29 00:00:53 -0400

    Update utils

--- src/deps/cli_utils.ts
+++ src/deps/cli_utils.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/utils@2.14.1/cli_utils.ts";
+export * from "https://deno.land/x/utils@2.14.2/cli_utils.ts";
--- src/deps/typebox.ts
+++ src/deps/typebox.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/utils@2.14.1/deps/typebox.ts";
+export * from "https://deno.land/x/utils@2.14.2/deps/typebox.ts";
