d7cc660a16bd81340b8bef19668824d74b9f9c4a
Jacky Nguyen <nktpro@gmail.com>
2024-03-08 14:20:49 -0500

    Update utils

--- src/deps/cli_utils.ts
+++ src/deps/cli_utils.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/utils@2.18.6/cli_utils.ts";
+export * from "https://deno.land/x/utils@2.18.7/cli_utils.ts";
--- src/deps/exec_utils.ts
+++ src/deps/exec_utils.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/utils@2.18.6/exec_utils.ts";
+export * from "https://deno.land/x/utils@2.18.7/exec_utils.ts";
